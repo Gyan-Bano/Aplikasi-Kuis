b965ca4e30fd28027cee6e36bfebd8e03e9387cf
Gyan Bano <gyanamurtiadhikarabano@gmail.com>
2024-08-04 20:53:46 +0700

    hide option in navbar for no auth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,12 +52,17 @@ const Navbar = () => {
     setMenuOpen(false); // Close the menu on mobile when navigating
   };
 
+  const handleCompletedQuizzesClick = () => {
+    navigate("/completed-quizzes");
+    setMenuOpen(false); // Close the menu on mobile when navigating
+  };
+
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
   };
 
   return (
-    <nav className="bg-white p-4 ">
+    <nav className="bg-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center space-x-4">
           <div
@@ -73,16 +78,30 @@ const Navbar = () => {
               QuizTrivia
             </span>
           </div>
-          <button
-            onClick={handleQuizListClick}
-            className={`hidden md:inline-block text-lg font-semibold font-poppins px-4 py-2 rounded ${
-              location.pathname === "/quiz-list"
-                ? "bg-indigo-600 text-white"
-                : "text-indigo-600 border-indigo-600 hover:bg-indigo-100 hover:text-indigo-800 focus:bg-indigo-600 focus:text-white"
-            }`}
-          >
-            Quiz List
-          </button>
+          {userLoggedIn && (
+            <>
+              <button
+                onClick={handleQuizListClick}
+                className={`hidden md:inline-block text-lg font-semibold font-poppins px-4 py-2 rounded ${
+                  location.pathname === "/quiz-list"
+                    ? "bg-indigo-600 text-white"
+                    : "text-indigo-600 border-indigo-600 hover:bg-indigo-100 hover:text-indigo-800 focus:bg-indigo-600 focus:text-white"
+                }`}
+              >
+                Quiz List
+              </button>
+              <button
+                onClick={handleCompletedQuizzesClick}
+                className={`hidden md:inline-block text-lg font-semibold font-poppins px-4 py-2 rounded ${
+                  location.pathname === "/completed-quizzes"
+                    ? "bg-indigo-600 text-white"
+                    : "text-indigo-600 border-indigo-600 hover:bg-indigo-100 hover:text-indigo-800 focus:bg-indigo-600 focus:text-white"
+                }`}
+              >
+                Completed Quizzes
+              </button>
+            </>
+          )}
         </div>
         <div className="hidden md:block">
           <button
@@ -126,16 +145,30 @@ const Navbar = () => {
       </div>
       {isMenuOpen && (
         <div className="md:hidden mt-2">
-          <button
-            onClick={handleQuizListClick}
-            className={`block w-full text-left text-lg font-semibold font-poppins px-4 py-2 rounded ${
-              location.pathname === "/quiz-list"
-                ? "bg-indigo-600 text-white"
-                : "text-indigo-600 border-indigo-600 hover:bg-indigo-100 hover:text-indigo-800 focus:bg-indigo-600 focus:text-white"
-            }`}
-          >
-            Quiz List
-          </button>
+          {userLoggedIn && (
+            <>
+              <button
+                onClick={handleQuizListClick}
+                className={`block w-full text-left text-lg font-semibold font-poppins px-4 py-2 rounded ${
+                  location.pathname === "/quiz-list"
+                    ? "bg-indigo-600 text-white"
+                    : "text-indigo-600 border-indigo-600 hover:bg-indigo-100 hover:text-indigo-800 focus:bg-indigo-600 focus:text-white"
+                }`}
+              >
+                Quiz List
+              </button>
+              <button
+                onClick={handleCompletedQuizzesClick}
+                className={`block w-full text-left text-lg font-semibold font-poppins px-4 py-2 rounded mt-2 ${
+                  location.pathname === "/completed-quizzes"
+                    ? "bg-indigo-600 text-white"
+                    : "text-indigo-600 border-indigo-600 hover:bg-indigo-100 hover:text-indigo-800 focus:bg-indigo-600 focus:text-white"
+                }`}
+              >
+                Completed Quizzes
+              </button>
+            </>
+          )}
           <button
             onClick={handleAuthClick}
             className={`block w-full text-left text-lg font-semibold font-poppins px-4 py-2 rounded mt-2 ${
