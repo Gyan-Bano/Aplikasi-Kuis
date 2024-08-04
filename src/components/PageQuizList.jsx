38bf47cca248c3f04bff267a970577d932015921
Gyan Bano <gyanamurtiadhikarabano@gmail.com>
2024-08-04 14:45:47 +0700

    search bar functional

--- src/components/PageQuizList.jsx
+++ src/components/PageQuizList.jsx
@@ -11,6 +11,7 @@ const PageQuizList = () => {
   const [loading, setLoading] = useState(true);
   const [confirmationOpen, setConfirmationOpen] = useState(false);
   const [selectedQuiz, setSelectedQuiz] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
   const { userLoggedIn } = useAuth();
   const navigate = useNavigate();
 
@@ -28,7 +29,7 @@ const PageQuizList = () => {
           id: doc.id,
           ...doc.data(),
         }));
-        console.log(quizData)
+        console.log(quizData);
         setQuizzes(quizData);
       } catch (error) {
         console.error("Error fetching quiz data:", error);
@@ -54,6 +55,14 @@ const PageQuizList = () => {
     setConfirmationOpen(false);
   };
 
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredQuizzes = quizzes.filter((quiz) =>
+    quiz.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   if (loading) {
     return (
       <div className="flex justify-center items-center h-[calc(100vh-80px)]">
@@ -71,11 +80,13 @@ const PageQuizList = () => {
         <input
           type="text"
           placeholder="Search for a quiz..."
+          value={searchQuery}
+          onChange={handleSearchChange}
           className="w-full px-4 py-2 border font-poppins rounded-lg focus:outline-none focus:border-indigo-500"
         />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {quizzes.map((quiz) => (
+        {filteredQuizzes.map((quiz) => (
           <div
             key={quiz.id}
             className="bg-white shadow-md rounded-lg p-4 flex flex-col items-center"
